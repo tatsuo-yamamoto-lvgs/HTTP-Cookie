def3ea04758658f36b4a878c4589447d049cfb98
tatsuo-yamamoto-lvgs <tatsuo.yamamoto@leverages.jp>
2024-01-24 19:29:29 +0900

    :+1:非同期処理を実装

--- connect-db.ts
+++ connect-db.ts
@@ -1,6 +1,5 @@
 import mysql, { Connection, FieldPacket, RowDataPacket } from "mysql2";
 
-// データベース接続設定
 const connection: Connection = mysql.createConnection({
   host: "127.0.0.1",
   port: 3306,
@@ -9,16 +8,6 @@ const connection: Connection = mysql.createConnection({
   database: "cookie-assignment",
 });
 
-// データベースへの接続
-connection.connect((error: mysql.QueryError | null) => {
-  if (error) {
-    console.error("データベース接続エラー: " + error.stack);
-    return;
-  }
-  console.log("データベースに接続されました。");
-});
-
-// テーブルの作成
 const createUserTableQuery: string = `
 CREATE TABLE IF NOT EXISTS user (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -35,35 +24,39 @@ CREATE TABLE IF NOT EXISTS post (
     FOREIGN KEY (user_id) REFERENCES user(id)
 )`;
 
-connection.query(
-  createUserTableQuery,
-  (
-    error: mysql.QueryError | null,
-    results: RowDataPacket[],
-    fields: FieldPacket[]
-  ) => {
-    if (error) {
-      console.error("ユーザーテーブル作成エラー: " + error.stack);
-      return;
-    }
-    console.log("ユーザーテーブルが作成されました。");
-  }
-);
+function createTable(query: string) {
+  return new Promise((reject) => {
+    connection.query(query, (error: mysql.QueryError | null) => {
+      if (error) {
+        reject("テーブル作成エラー: " + error.stack);
+      }
+    });
+  });
+}
 
-connection.query(
-  createPostTableQuery,
-  (
-    error: mysql.QueryError | null,
-    results: RowDataPacket[],
-    fields: FieldPacket[]
-  ) => {
-    if (error) {
-      console.error("ポストテーブル作成エラー: " + error.stack);
-      return;
-    }
-    console.log("ポストテーブルが作成されました。");
-  }
-);
+function connectDatabase() {
+  return new Promise((reject) => {
+    connection.connect((error: mysql.QueryError | null) => {
+      if (error) {
+        reject("データベース接続エラー: " + error.stack);
+      }
+    });
+  });
+}
 
-// データベース接続終了
-connection.end();
+async function main(): Promise<void> {
+  try {
+    await connectDatabase();
+    console.log("データベース接続成功");
+    await createTable(createUserTableQuery);
+    console.log("userテーブル作成成功");
+    await createTable(createPostTableQuery);
+    console.log("postテーブル作成成功");
+  } catch (error) {
+    console.error(error);
+  } finally {
+    connection.end();
+  }
+}
+
+main();
