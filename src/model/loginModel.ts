3c66c89f82e73820d7fc8e2887ac4d35cf6f020c
tatsuo-yamamoto-lvgs <tatsuo.yamamoto@leverages.jp>
2024-01-30 19:09:53 +0900

    とりあえず.envで

--- src/model/loginModel.ts
+++ src/model/loginModel.ts
@@ -1,14 +1,16 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
 
+dotenv.config();
 export async function loginModel(username: string): Promise<string | null> {
   try {
     // データベース接続設定
     const connection = await mysql.createConnection({
-      host: "127.0.0.1",
-      port: 3306,
-      user: "root",
-      password: "cookie-assignment-pw",
-      database: "cookie-assignment",
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
     });
 
     // SQLクエリの実行
